3dad398c17bfe9748b687e0178d12f2553e5864e
Eddie Aloja <alojaeddie@gmail.com>
2022-10-05 22:13:58 +0100

    added express to website

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express();
+
+
+app.use(express.static(__dirname))
+
+app.listen(3000, function(){
+    console.log("The server is running on port 3000")
+})
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
